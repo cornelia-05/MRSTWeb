18e36de7f32043072a321b7f85d4c1c7a9d4005b
cornelia-05 <cornelia.cojocaru@isa.utm.md>
2025-04-01 08:09:15 +0300

    Sql update

---
+++ BusinessLogic/Core/UserContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.User;
+using BusinessLogic.Models;
+using System.Runtime.Remoting.Contexts;
+using System.Data.Entity;
+
+
+
+namespace BusinessLogic.Core
+{
+     public class UserContext : DbContext
+     {
+          public DbSet<UDbTable> UDbTables { get; set; }  
+          public DbSet<User> Users { get; set; } 
+          public DbSet<Service> Services { get; set; }  
+
+          public UserContext() : base("name=MRSTWebDB") { }
+
+          protected override void OnModelCreating(DbModelBuilder modelBuilder)
+          {
+               base.OnModelCreating(modelBuilder);
+
+          }
+     }
+}
---
+++ BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DBModel
+{
+     public class UserContext : DbContext
+     {
+          public UserContext()
+              : base("name=eUseControl") // connection string name defined in Web.config
+          {
+          }
+
+          public virtual DbSet<UDbTable> Users { get; set; }
+     }
+}
